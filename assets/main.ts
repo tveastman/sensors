7348bf82aee9860e4e6ec15f92671eadddfd323e
Tom Eastman <tom@eastman.nz>
2024-04-25 09:08:37 +1200

    Add filtering

--- assets/main.ts
+++ assets/main.ts
@@ -1,26 +1,28 @@
-import Chart, { ChartItem } from 'chart.js/auto';
+// import Chart, { ChartItem } from 'chart.js/auto';
 
-export function hello() {
-    const ctx = document.getElementById('myChart')!
+// export function hello() {
+//     const ctx = document.getElementById('myChart')!
 
 
-    new Chart(ctx, {
-        type: 'bar',
-        data: {
-          labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-          datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            borderWidth: 1
-          }]
-        },
-        options: {
-          scales: {
-            y: {
-              beginAtZero: true
-            }
-          }
-        }
-      })
+//     new Chart(ctx, {
+//         type: 'bar',
+//         data: {
+//           labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+//           datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             borderWidth: 1
+//           }]
+//         },
+//         options: {
+//           scales: {
+//             y: {
+//               beginAtZero: true
+//             }
+//           }
+//         }
+//       })
     
-}
+// }
+
+
